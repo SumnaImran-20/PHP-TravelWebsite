897268cd96e22111f9133822dc90682614418fa1
Sumna Imran <88527179+SumnaImran-20@users.noreply.github.com>
2022-06-08 21:42:25 +0500

    Add files via upload

---
+++ book_db.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 08, 2022 at 05:20 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `book_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_form`
+--
+
+CREATE TABLE `book_form` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `guests` int(255) NOT NULL,
+  `arrivals` date NOT NULL,
+  `leaving` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `book_form`
+--
+
+INSERT INTO `book_form` (`id`, `name`, `email`, `phone`, `address`, `location`, `guests`, `arrivals`, `leaving`) VALUES
+(1, 'sumna Imran', 'sumna.imran87@gmail.com', '123456789', '338/15 F.B Area ,karachi', 'hawaii', 7, '2022-06-14', '2022-06-20'),
+(2, 'anas bhai', 'abc@gmail.com', '12345', 'abc road karachi', 'malaysia', 3, '2022-06-09', '2022-06-11'),
+(3, 'taha', 'taha@gmail.com', '12345', '330/15 F.B Area ,karachi', 'singapore', 5, '2022-06-10', '2022-06-11');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book_form`
+--
+ALTER TABLE `book_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book_form`
+--
+ALTER TABLE `book_form`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
